24bb3ef3ef956bd50c6496cfe3ad3d9aec1d9ab2
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-04 22:16:07 +0200

    Added restclient library

---
+++ RESTApi/RESTApiLibrary/RESTClient.cs
@@ -0,0 +1,35 @@
+﻿using RESTApiLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+namespace RESTApiLibrary
+{
+    public class RESTClient
+    {
+        private HttpClient httpClient;
+        public RESTClient()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://www.localhost:5001");
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await httpClient.GetFromJsonAsync<List<User>>("api/users");
+        }
+
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            return await httpClient.GetFromJsonAsync<User>($"api/users/{id}");
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            return await httpClient.PostAsJsonAsync<User>("api/users", user);
+        }
+    }
+}
