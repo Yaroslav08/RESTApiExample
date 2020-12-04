c60308e3b67539e09598762ff1adda4c183b98df
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-04 21:32:49 +0200

    Added UserController

---
+++ RESTApi/RESTApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTApi.Models;
+using RESTApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RESTApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAllUsers() => Ok(_userService.GetAllUsers());
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(string id) => Ok(_userService.GetUserById(id));
+
+
+        [HttpGet("search")]
+        public IActionResult GetUsersByName(string name) => Ok(_userService.GetUserByName(name));
+
+        [HttpPost()]
+        public IActionResult CreateUser([FromBody] User user) => Ok(_userService.CreateUser(user));
+    }
+}
