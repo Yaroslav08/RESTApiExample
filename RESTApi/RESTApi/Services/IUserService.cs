80272b81a68f69473e15e2b2c051829120cdd911
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-04 21:19:39 +0200

    Added IUserService interface

---
+++ RESTApi/RESTApi/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using RESTApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTApi.Services
+{
+    public interface IUserService
+    {
+        User GetUserById(string id);
+        List<User> GetAllUsers();
+        List<User> GetUserByName(string name);
+        User CreateUser(string id);
+    }
+}
