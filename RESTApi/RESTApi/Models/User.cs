39875a26633d1a1a7218bdbbdb46a469b8df6b12
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-04 21:17:38 +0200

    Added User entity

---
+++ RESTApi/RESTApi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RESTApi.Models
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public DateTime CreayedAt { get; set; } = DateTime.Now;
+        public string Name { get; set; }
+
+        public User(string name) => Name = name;
+    }
+}
