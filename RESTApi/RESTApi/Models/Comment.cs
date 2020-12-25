4aa5e2f719db05187e659ba97bbb3c201c512d01
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-25 16:26:26 +0200

    Added Comment entity

---
+++ RESTApi/RESTApi/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RESTApi.Models
+{
+    public class Comment
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string Text { get; set; }
+    }
+}
